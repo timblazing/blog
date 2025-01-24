b20627509f1c8713569c013305ba58f945e5ff47
Timothy (Clay) Blasingame <timothy.blasingame@okstate.edu>
2025-01-24 16:46:18 -0600

    remove astro expressive code

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,11 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import pagefind from "astro-pagefind";
 
-import expressiveCode from "astro-expressive-code";
-
 export default defineConfig({
   site: "https://timblazing.site",
   base: "/",
   integrations: [
     tailwind(),
-    expressiveCode({
-      themes: ['aurora-x'],
-      styleOverrides: {
-        borderRadius: '0.5rem',
-        frames: {
-          shadowColor: '#124',
-        },
-        codeBlock: {
-          width: '100%',
-          maxWidth: 'calc(100vw - 2rem)',
-          overflowX: 'auto',
-          wordBreak: 'break-word',
-          whiteSpace: 'pre-wrap'
-        }
-      },
-    }),
     mdx(),
     sitemap(),
     pagefind()
