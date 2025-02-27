676ececf2a49f23fc598fd074e86d23810013877
Timothy (Clay) Blasingame <timothy.blasingame@okstate.edu>
2025-02-26 21:03:07 -0600

    started adding my projects

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -5,9 +5,8 @@ import { getFilteredCollectionEntries, resolvePath, sortByLastUpdateDate } from 
 export async function GET(context) {
   const blog = (await getFilteredCollectionEntries("blog")).entries;
   const projects = (await getFilteredCollectionEntries("projects")).entries;
-  const talks = (await getFilteredCollectionEntries("talks")).entries;
 
-  const items = [...blog, ...projects, ...talks].sort(
+  const items = [...blog, ...projects].sort(
     sortByLastUpdateDate,
   );
 
